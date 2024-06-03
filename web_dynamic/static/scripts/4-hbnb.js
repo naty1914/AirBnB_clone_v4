e83a74522c59738651d8c96c6b5f698e3c5e7a5e
santaETH <sdamissie@yahoo.com>
2024-06-03 01:57:21 -0700

    web_dynamic

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -24,58 +24,57 @@ window.addEventListener('load', function () {
   });
 
   // task 4
-  $.ajax({
-    type: 'POST',
-    url: 'http://0.0.0.0:5001/api/v1/places_search/',
-    contentType: 'application/json',
-    data: JSON.stringify({})
-  }).done(function (data) {
-    for (const place of data) {
-      const template = `<article>
-
+  $('.filters button').click(function () {
+    $.ajax({
+      type: 'POST',
+      url: 'http://0.0.0.0:5001/api/v1/places_search/',
+      contentType: 'application/json',
+      data: JSON.stringify({ amenities: Object.keys(amenityIds) })
+    }).done(function (data) {
+      $('section.places').empty();
+      $('section.places').append('<h1>Places</h1>');
+      for (const place of data) {
+        const template = `<article>
         <div class="title">
-
-          <h2>${place.name}</h2>
-
-          <div class="price_by_night">
-
-        $${place.price_by_night}
-
-          </div>
+        <h2>${place.name}</h2>
+        <div class="price_by_night">
+      $${place.price_by_night}
+      </div>
         </div>
         <div class="information">
-          <div class="max_guest">
+        <div class="max_guest">
         <i class="fa fa-users fa-3x" aria-hidden="true"></i>
 
         <br />
 
-        ${place.max_guest} Guests
+      ${place.max_guest} Guests
 
-          </div>
-          <div class="number_rooms">
+      </div>
+        <div class="number_rooms">
         <i class="fa fa-bed fa-3x" aria-hidden="true"></i>
 
         <br />
 
-        ${place.number_rooms} Bedrooms
-          </div>
-          <div class="number_bathrooms">
+      ${place.number_rooms} Bedrooms
+      </div>
+        <div class="number_bathrooms">
         <i class="fa fa-bath fa-3x" aria-hidden="true"></i>
 
         <br />
 
-        ${place.number_bathrooms} Bathroom
+      ${place.number_bathrooms} Bathroom
 
-          </div>
+      </div>
         </div>
         <div class="description">
 
-          ${place.description}
+      ${place.description}
 
-        </div>
+      </div>
 
       </article> <!-- End 1 PLACE Article -->`;
-      $('section.places').append(template);
-    }
+        $('section.places').append(template);
+      }
+    });
   });
 });
